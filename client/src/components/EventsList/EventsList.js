548b8e07b3949734201fb9a7e8abc725e7a9d95c
forkhelloworld <serhiisliusarskyi@gmail.com>
2025-02-12 17:20:22 +0200

    feat: add sorting events

--- client/src/components/EventsList/EventsList.js
+++ client/src/components/EventsList/EventsList.js
@@ -2,10 +2,15 @@ import React from 'react'
 import EventItem from '../EventItem/EventItem'
 
 const EventsList = ({ timers, removeTimer }) => {
+  const sortedTimers = [...timers].sort((a, b) => a.eventTime - b.eventTime)
   return (
     <ul className='timer-list'>
-      {timers.map(timer => (
-        <EventItem key={timer.id} timer={timer} removeTimer={removeTimer} />
+      {sortedTimers.map(timer => (
+        <EventItem
+          key={timer.id}
+          timer={timer}
+          removeTimer={removeTimer}
+        />
       ))}
     </ul>
   )
