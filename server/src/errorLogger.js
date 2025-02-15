1b445065cb632ceee4a430b22733fc698fbef1ec
forkhelloworld <serhiisliusarskyi@gmail.com>
2025-02-15 17:28:55 +0200

    add schedule logs backup

--- server/src/errorLogger.js
+++ server/src/errorLogger.js
@@ -1,10 +1,11 @@
 const fs = require('fs/promises')
 const path = require('path')
+const cron = require('node-cron')
 const CONSTANTS = require('./constants')
 
 module.exports.logError = error => {
   const code = error.code || 500
-  const logFilePath = path.resolve(CONSTANTS.LOGS_PATH)
+  const logFilePath = path.resolve(CONSTANTS.LOGS_FILE_PATH)
   const logEntry = {
     message: error.message || 'Unknown error',
     time: Date.now(),
@@ -31,3 +32,43 @@ module.exports.logError = error => {
     )
   })
 }
+
+const backup = async () => {
+  const logFilePath = path.resolve(CONSTANTS.LOGS_FILE_PATH)
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-')
+  const backupFilePath = path.resolve(
+    CONSTANTS.LOGS_PATH,
+    `backup-${timestamp}.json`
+  )
+
+  try {
+    const data = await fs.readFile(logFilePath, 'utf-8')
+    let logs
+    try {
+      logs = JSON.parse(data)
+    } catch (err) {
+      console.error('Error parsing file:', err)
+      return
+    }
+
+    const transformedLogs = logs.map(log => ({
+      message: log.message,
+      code: log.code,
+      time: log.time
+    }))
+
+    await fs.writeFile(
+      backupFilePath,
+      JSON.stringify(transformedLogs, null, 2),
+      'utf8'
+    )
+
+    await fs.writeFile(logFilePath, '[]', 'utf8')
+  } catch (error) {
+    console.error('Error processing logs:', error)
+  }
+}
+
+cron.schedule('0 0 * * *', async () => {
+  await backup()
+})
