295ffbbcec780d87dac38f0bf3fab328cf7e6a8d
forkhelloworld <serhiisliusarskyi@gmail.com>
2025-02-15 12:43:24 +0200

    add conversations and conversation participants tables

--- task6.sql
+++ task6.sql
@@ -1,3 +1,14 @@
+CREATE TABLE IF NOT EXISTS conversations (
+  id int PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS conversation_participants (
+  chat_id int REFERENCES conversations ON DELETE CASCADE,
+  user_id int REFERENCES "Users",
+  is_blocked BOOLEAN DEFAULT FALSE,
+  is_favorite BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (chat_id, user_id)
+);
 
 CREATE TABLE IF NOT EXISTS messages (
     conversation int REFERENCES conversations,
@@ -6,7 +17,7 @@ CREATE TABLE IF NOT EXISTS messages (
 );
 
 CREATE TABLE IF NOT EXISTS catalogs (
-    userId int REFERENCES "Users",
-    catalogName varchar(256) NOT NULL CHECK (catalogName != ''),
+    user_id int REFERENCES "Users",
+    name varchar(256) NOT NULL CHECK (catalogName != ''),
     chat int REFERENCES conversations
 );
