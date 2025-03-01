7bb592eaccb812fbfb7d0cfe0af5ae49ba93145a
forkhelloworld <serhiisliusarskyi@gmail.com>
2025-03-01 09:57:25 +0200

    fix: seeders

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -4,14 +4,14 @@ module.exports = {
       'banks',
       [
         {
-          cardNumber: '4564654564564564',
+          card_number: '4564654564564564',
           name: 'SquadHelp',
           expiry: '11/27',
           cvc: '453',
           balance: 0
         },
         {
-          cardNumber: '4111111111111111',
+          card_number: '4111111111111111',
           name: 'yriy',
           expiry: '09/27',
           cvc: '505',
